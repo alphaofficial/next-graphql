3f9af3074fd87f71a8954b1390b06cdfa74f6485
Albert Akrong <alphaxsalt@gmail.com>
2023-03-09 21:53:25 +0000

    update readme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import {
@@ -7,6 +6,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
